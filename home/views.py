05cf5cb05049afedc652ae668dff2dea4caa7c5e
ryanlkim <89424093+ryanlkim@users.noreply.github.com>
2025-08-15 09:45:36 -0400

    31620

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def resturant_view(request):
+    context = {
+        'title':'Our Resturant',
+        'description':'Welcome to our exquisite resturant where culinary artistry meets warm hospitality...',
+        'cuisine':'We specialize in authentic Asian cuisine',
+        'atmosphere':'Enjoy your meal in our elegantly designed space...'
+    }
+    return render(request, 'home/resturant.html', context)
