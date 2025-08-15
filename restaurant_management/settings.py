9001c52e067649bb1efb2af19afc81d22714462d
ryanlkim <89424093+ryanlkim@users.noreply.github.com>
2025-08-15 09:53:16 -0400

    31619

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RESTURANT_CONFIG = {
+    'NAME': 'Resturant Name',
+}
