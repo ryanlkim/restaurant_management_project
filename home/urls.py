6f9638955f0608dcf51d91af12a0fd9ed4af64e3
ryanlkim <89424093+ryanlkim@users.noreply.github.com>
2025-08-17 13:38:52 -0400

    33655

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('restaurant/', views.restaurant_view,name='restaurant')
+    path('restaurant/', views.restaurant_view,name='restaurant'),
+    path('contact/', views.contact_us, name='contact-us'),
 ]
